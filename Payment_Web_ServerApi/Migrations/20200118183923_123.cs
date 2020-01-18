9c0cc1416d434ac0c7a1c9beff21a4ec85f3077a
xchen <xchen4791@gmail.com>
2020-01-18 17:50:44 -0500

    initial commit

---
+++ Payment_Web_ServerApi/Migrations/20200118183923_123.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Payment_Web_ServerApi.Migrations
+{
+    public partial class _123 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationData",
+                table: "PaymentDetails",
+                newName: "ExpirationDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                newName: "ExpirationData");
+        }
+    }
+}
---
+++ Payment_Web_ServerApi/Models/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment_Web_ServerApi.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        /*options = pass in db provider such as SQL and connection string...*/
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+             
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
